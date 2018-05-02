71b3b624f08f9ee5ef5dca3abc415b5e3c931368
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-05-02 17:44:27 +0200

    Initialize insults as a service

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sources
+Lutheran insults: http://ergofabulous.org/luther/insult-list.php
+100 greatest insults: https://thoughtcatalog.com/lorenzo-jensen-iii/2016/11/sick-burns-the-100-greatest-insults-of-all-time/
---
+++ insults.py
@@ -0,0 +1,32 @@
+import markovify
+import re
+import spacy
+
+
+nlp = spacy.load("en_core_web_sm")
+
+
+class POSifiedText(markovify.Text):
+    def word_split(self, sentence):
+        return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
+
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
+
+
+with open("luther.txt") as f:
+    luther = f.read()
+
+with open("insults.txt") as f:
+    insults = f.read()
+
+text_model_luther = markovify.Text(luther, state_size=3)
+text_model_insults = markovify.Text(insults, state_size=3)
+
+model_combo = markovify.combine([text_model_luther, text_model_insults], [1.5, 1])
+
+for i in range(5):
+    print(model_combo.make_short_sentence(140, tries=25))
+# def generate_insult(name: str):
+#    """Some you hates deserves only the very best insults"""
